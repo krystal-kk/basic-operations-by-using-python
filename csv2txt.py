bb2b5f343f760395ed2217567970f3adf83e75f3
krystal-kk <81208705+krystal-kk@users.noreply.github.com>
2021-04-13 16:02:37 +0900

    Update csv2txt.py

--- csv2txt.py
+++ csv2txt.py
@@ -1,7 +1,11 @@
-import csv
+import pandas as pd
+import os
+ 
+ 
+data = pd.read_csv('/home/jaran/Documents/ISIC2017/Reocrd/Train.csv', encoding='utf-8')
+with open('/home/jaran/Documents/ISIC2017/Reocrd/Train1.txt','a+', encoding='utf-8') as f:
+    for line in data.values:
+        f.write((str(line[0])+' '+str(line[1])+'\n'))
 
-output=open('xyz.txt','w')
-
-with open('abc.csv',"rt", encoding='ascii') as f:
-  for row in f:
-    output.write(row)
+        
+# the first row may miss
